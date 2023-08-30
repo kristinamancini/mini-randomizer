5b635c4ef6e630249a7d796a9bb54ed5e268f463
Kristina Mancini <km36558p@pace.edu>
2023-08-30 17:35:02 -0400

    Modify html - add one item and display it

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-const myButton = document.getElementById("add-item-button");
-myButton.onclick = () => {
-    alert("The add button was pressed");
-};
+// const myButton = document.getElementById("add-item-button");
+// myButton.onclick = () => {
+//     alert("The add button was pressed");
+       
+// };
 
 
