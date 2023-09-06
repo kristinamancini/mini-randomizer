33441814b79d1d462f3f42519dce261c7640b99d
Kristina Mancini <km36558p@pace.edu>
2023-09-06 15:10:03 -0400

    Format checkbox and labels, changed ID and clear all

--- scripts/main.js
+++ scripts/main.js
@@ -13,29 +13,61 @@ function addItem(){
     //add to list
     let data = [""];
     let list = document.getElementById("myList");
+    let myDiv = document.getElementById("myItems");
     var inputVal = document.getElementById("inputId").value;
 
     //for checkboxes
-    //let myDiv = document.getElementById("items-list");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
 
+    //var label = document.createElement("label");
+    //label.htmlFor = "id";
+
+    
 
 
     for (i = 0; i < data.length; i++){
-        let li = document.createElement("li");
+        let label = document.createElement("label");
+        var br = document.createElement("br");
         data[i] = inputVal;
-        li.innerText = data[i];
-        list.appendChild(li); 
-        //add checkbox
-        list.appendChild(checkbox);               
+        label.innerText = data[i];
+        //label.appendChild(document.createTextNode(i));
+        myDiv.appendChild(label);
+        myDiv.appendChild(checkbox);
+        myDiv.appendChild(br);
+        
+        
     }  
-
-
 }
 
+
+// function addItem(){
+//     //add to list
+//     let data = [""];
+//     let list = document.getElementById("myList");
+//     var inputVal = document.getElementById("inputId").value;
+
+//     //for checkboxes
+//     //let myDiv = document.getElementById("items-list");
+//     let checkbox = document.createElement("input");
+//     checkbox.type = "checkbox";
+
+//     for (i = 0; i < data.length; i++){
+//         let li = document.createElement("li");
+//         data[i] = inputVal;
+//         li.innerText = data[i];
+//         list.appendChild(li); 
+//         //add checkbox
+//         list.appendChild(checkbox);               
+//     }  
+// }
+
 function clearItems(){
-    document.getElementById("myList").innerHTML = "";
+    document.getElementById("myItems").innerHTML = "";
+    //document.getElementById("myList").innerHTML = "";
     myData.length = 0;
 }
 
