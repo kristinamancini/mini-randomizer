f32da92c2a47123306c0e80aa8ba21efdf172dbb
Kristina Mancini <km36558p@pace.edu>
2023-09-06 14:40:58 -0400

    Add a checkbox - need to format

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,5 @@
-// const myButton = document.getElementById("add-item-button");
-// myButton.onclick = () => {
-//     alert("The add button was pressed");
-       
-// };
-
 let myData = [];
+
 function itemList(){
     if (myData.length < 7){
         myData.push(document.getElementById("inputId").value);
@@ -15,15 +10,28 @@ function itemList(){
 }
 
 function addItem(){
+    //add to list
     let data = [""];
     let list = document.getElementById("myList");
     var inputVal = document.getElementById("inputId").value;
+
+    //for checkboxes
+    //let myDiv = document.getElementById("items-list");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+
+
     for (i = 0; i < data.length; i++){
         let li = document.createElement("li");
         data[i] = inputVal;
         li.innerText = data[i];
-        list.appendChild(li);                
+        list.appendChild(li); 
+        //add checkbox
+        list.appendChild(checkbox);               
     }  
+
+
 }
 
 function clearItems(){
